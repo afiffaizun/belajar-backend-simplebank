c10b66a9a79a812e610aaa43f43528aa5632ddb9
Muhammad Afif Faizun <afiffaizun31@gmail.com>
2025-05-14 19:55:31 +0700

    first commit

---
+++ src/main/java/in/techmafif/bankjago/model/Account.java
@@ -0,0 +1,5 @@
+package in.techmafif.bankjago.model;
+
+public class Account {
+
+}
---
+++ src/test/java/in/techmafif/bankjago/SimplebankApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techmafif.bankjago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
